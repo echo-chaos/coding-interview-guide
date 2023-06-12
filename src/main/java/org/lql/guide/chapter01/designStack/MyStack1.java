02765267fc15c934017f6fa9df76f11782a0245f
liangqinglong <1805686827@qq.com>
2023-06-12 22:47:01 +0800

    第一章 1 设计一个有getMin功能的栈

---
+++ src/main/java/org/lql/guide/chapter01/designStack/MyStack1.java
@@ -0,0 +1,45 @@
+package org.lql.guide.chapter01.designStack;
+
+import java.util.Stack;
+
+/**
+ * @author: liangqinglong
+ * @date: 2023-06-12 22:19
+ * @description: 设计一个有getMin功能的栈
+ */
+public class MyStack1 {
+
+  private Stack<Integer> stackData;
+
+  private Stack<Integer> stackMin;
+
+  public MyStack1() {
+    this.stackData = new Stack<>();
+    this.stackMin = new Stack<>();
+  }
+
+  public void push(int data) {
+    if (this.stackMin.isEmpty()) {
+      this.stackMin.push(data);
+    } else if (data <= this.getMin()) {
+      this.stackMin.push(data);
+    }
+    this.stackData.push(data);
+  }
+
+  public int pop() {
+    if (this.stackData.isEmpty()) {}
+
+    int value = this.stackData.pop();
+    if (value == this.getMin()) {
+      this.stackMin.pop();
+    }
+    return value;
+  }
+
+  public int getMin() {
+    if (this.stackMin.isEmpty()) {}
+
+    return this.stackMin.peek();
+  }
+}
---
+++ src/main/java/org/lql/guide/chapter01/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author: liangqinglong
+ * @date: 2023-06-12 22:03
+ * @description: 第一章 栈和队列
+ */
+package org.lql.guide.chapter01;
